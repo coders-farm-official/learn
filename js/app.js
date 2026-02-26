942a3c1ca9273bdf32b95f4601874be8601878c7
Kamron Kennedy <kamronkennedy@gmail.com>
2026-02-26 15:38:29 -0500

    Add donation link and message about unlocking side quests

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
    Initialization and page routing
    ======================================== */
 
-const SITE_VERSION = '1.18';
+const SITE_VERSION = '1.19';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Initialize theme first (prevents flash)
